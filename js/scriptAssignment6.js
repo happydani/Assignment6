af15bd63af53ad450f6e5b558a0bed8cd5ab35a1
Danette Klein <danetteklein@gmail.com>
2016-07-31 13:30:39 -0700

    Committing updates to Assignment6

--- js/scriptAssignment6.js
+++ js/scriptAssignment6.js
@@ -1,27 +1,27 @@
-/*global launchProgram, validateSimpleCharField, addressType*/
-var W3CDOM = (document.getElementsByTagName && document.createElement);
-var VALID_EMAIL_REGEX = "^[^\\W][a-zA-Z0-9\\_\\-\\.]+([a-zA-Z0-9\\_\\-\\.]+)*\\@[-a-zA-Z0-9_]+(\\.[a-zA-Z0-9_]+)*\\.[a-zA-Z]{2,4}$";
+/*global launchProgram, isSimpleAlpha, isSimpleNumber, isAddress, isState, isEmail, addressType, dir */
+
 document.addEventListener("DOMContentLoaded", launchProgram, false);
 /*----------------Initialize Customer Objects--------------*/
 function launchProgram() {
 	'use strict';
 	var customer = {}; // the customer object
-	
+
 	/*----------------Launch Customer Objects--------------*/
 	function launchCustomer() {
 		//make sure to handle otherAddressType differently
 		var myName = document.getElementById('myName'),
 			addressType = document.getElementById('addressType'),
 			otherAddressType = document.getElementById('otherAddressType'),
-			streetAddress1 = document.getElementById('streetAddress1'),
+			streetAddress = document.getElementById('streetAddress'),
 			aptSteRoom = document.getElementById('aptSteRoom'),
 			city = document.getElementById('city'),
 			state = document.getElementById('state'),
 			zip = document.getElementById('zip'),
 			phone = document.getElementById('phone'),
-			myEmail = document.getElementById('myEmail');
+			myEmail = document.getElementById('myEmail'),
+			btnBuildPizza = document.getElementById('btnBuildPizza');
 		/*----------------Hide/Show 'Other Address' Types--------------*/
-		function hideField() {
+		function hideOrShowOtherField() {
 
 			var x = document.getElementById("addressType").value;
 			if (x === "other") {
@@ -30,56 +30,245 @@ function launchProgram() {
 				document.getElementById("hideOtherAddressType").style.display = 'none';
 			}
 		}
-		//window.console.log(hideField);
-		//window.console.dir(myName);
-		myName.onblur = function () {
-			window.console.log(myName.value);
-			var myInput = myName.value;
-			if (validateSimpleAlphaCharField(myInput) === true) {
-				customer.myName = myInput;
+
+		dir(myName);
+		myName.onkeyup = function () {
+			if (isSimpleAlpha(myName.value)) {
+				customer.myName = myName.value;
+				myName.nextSibling.innerHTML = '';
 			} else {
-				// it's false so create html message "invalid input, please use alphabetic characters A-Z"
-				document.getElementById("div")
-				document.getElementById().myName.id.innerHTML('Oops! Please only use alphabetic characters for your name');
+				myName.nextSibling.innerHTML = ' Oops! Please use alphabetic characters';
 			}
 		};
-		addressType.addEventListener("change", hideField, false);
+		
+		
+		addressType.onchange = function () {
+			dir('addressType[0].selected =');
+			dir(addressType[0].selected);
+			if (addressType[0].selected !== true) {
+//				customer.otherAddressType = otherAddressType.value;
+				addressType.nextSibling.innerHTML = '';
+			} else {
+				addressType.nextSibling.innerHTML = ' Oops! Please select your type of residence';
+			}
+		};
+		
+		dir(otherAddressType);
+		otherAddressType.onkeyup = function () {
+			if (isSimpleAlpha(otherAddressType.value)) {
+				customer.otherAddressType = otherAddressType.value;
+				otherAddressType.nextSibling.innerHTML = '';
+			} else {
+				otherAddressType.nextSibling.innerHTML = ' Oops! Please use alphabetic characters';
+			}
+		};
+
+		//	validate Address Type and assure all required fields have input
+		btnBuildPizza.onclick = function () {
+			var i;
+			/*if (myName.value === '') {
+				myName.nextSibling.innerHTML = ' Oops! This field is required now';
+			} else {
+				myName.nextSibling.innerHTML = '';
+			}*/
+			dir(addressType);
+
+			if (addressType['0'].selected) {
+				addressType.nextSibling.innerHTML = ' Oops! Please select your type of residence';
+
+			} else {
+				addressType.nextSibling.innerHTML = '';
+				for (i = 0; i < addressType.length; i += 1) {
+					if (addressType[i].selected) {
+						if (i === 7) { //selection is 'Other'
+							dir(i);
+							dir('otherAddressType.value =');
+							dir(otherAddressType.value);
+							if (isAddress(otherAddressType.value)) {
+								customer.otherAddressType = otherAddressType.value;
+								addressType.nextSibling.innerHTML = '';
+							} else {
+								otherAddressType.nextSibling.innerHTML = ' Oops! Please use alphabetic characters';
+							}
+						} else { // i === 1 thru 6
+							customer.addressType = addressType[i].value;
+							addressType.nextSibling.innerHTML = '';
+							break;
+						}
+					}
+				}
+			}
+
+//			dir(customer);
+//			dir(customer.addressType);
+//			dir(addressType[i].value);
+			
+			
+			if (myName.value === '') {
+				myName.nextSibling.innerHTML = ' Oops! This field is required';
+			} else {
+				myName.nextSibling.innerHTML = '';
+			}
+			if (streetAddress.value === '') {
+				streetAddress.nextSibling.innerHTML = ' Oops! This field is required';
+			} else {
+				streetAddress.nextSibling.innerHTML = '';
+			}
+			if (city.value === '') {
+				city.nextSibling.innerHTML = ' Oops! This field is required';
+			} else {
+				city.nextSibling.innerHTML = '';
+			}
+			if (state.value === '') {
+				state.nextSibling.innerHTML = ' Oops! This field is required';
+			} else {
+				state.nextSibling.innerHTML = '';
+			}
+			if (zip.value === '') {
+				zip.nextSibling.innerHTML = ' Oops! This field is required';
+			} else {
+				zip.nextSibling.innerHTML = '';
+			}
+			if (phone.value === '') {
+				phone.nextSibling.innerHTML = ' Oops! This field is required';
+			} else {
+				phone.nextSibling.innerHTML = '';
+			}
+			if (myEmail.value === '') {
+				myEmail.nextSibling.innerHTML = ' Oops! This field is required';
+			} else {
+				myEmail.nextSibling.innerHTML = '';
+			}
+		}; // end validating Address Type
+
+		// determine whether to show otherAddressType input field
+		addressType.addEventListener("change", hideOrShowOtherField, false);
+
+
+		dir(streetAddress);
+		streetAddress.onkeyup = function () {
+			var myInput = streetAddress.value;
+			if (isAddress(myInput)) {
+				customer.streetAddress = myInput;
+				streetAddress.nextSibling.innerHTML = '';
+			} else {
+				streetAddress.nextSibling.innerHTML = ' Oops! Please use alphanumeric characters';
+			}
+		};
+		
+		dir(city);
+		city.onkeyup = function () {
+			if (isSimpleAlpha(city.value)) {
+				customer.city = city.value;
+				city.nextSibling.innerHTML = '';
+			} else {
+				city.nextSibling.innerHTML = ' Oops! Please use alphabetic characters';
+			}
+		};
+
+		dir(state);
+		state.onkeyup = function () {
+			if (isState(state.value)) {
+				customer.state = state.value;
+				state.nextSibling.innerHTML = '';
+			} else {
+				state.nextSibling.innerHTML = ' Oops! Please use two alphabetic characters abbreviation for your state';
+			}
+		};
 		// populate remaining form fields
 	} /* end launchCustomer */
-	
-	function validateSimpleCharField(input) {
-		// declare local variables 
-		// including a boolean (validInput) initially set to true 
-		var validInput = true, //need a var to hold the regex equation
-			regex = '^[A-Za-z]$';
-		//test whether 'input' has valid alpha characters (and no numbers etc)
-		// if input is valid (test against regex)
-		if (input === regex) {
-			//create a property for (input) in the Customer object for future use will take place in calling function
+
+
+	/*--------------------- Form Validation ---------------------*/
+
+	function isSimpleAlpha(input) {
+		dir('entered isSimpleAlpha ... input = ');
+		dir(input);
+		var validInput,
+			letters = /^[A-Za-z\. ]+$/; // '+' assure there's at least one char in the string
+		dir(input.match(letters));
+		if (input.match(letters) !== null) {
 			validInput = true;
 		} else {
 			validInput = false;
 		}
-		//return (validInput) that says whet her valid or result 
+		dir('valid simple alpha input =');
+		dir(validInput);
 		return validInput;
 	}
-
-	function validateSimpleNumberField(input) {
-		// declare local variables 
-		// including a boolean (validInput) initially set to true 
-		// and one for assigning the input to a one with local scope if I plan on saving it (which I think I should so I can pass it back later to billing if the customer checks it's the same as the customer's contact info)
-		var validInput = true;
-		//test whether 'input' has valid alpha characters (and no numbers etc)
-		// if input is valid create an Object for that input
-		// if it's false create html message "invalid input, please use alphabetic characters A-Z"
-		if (validateSimpleCharField(input)) {
+	
+	function isState(input) {
+		dir('entered isState ... input = ');
+		dir(input);
+		var validInput,
+			state = /^[A-Za-z]{2}$/;
+		dir(input.match(state));
+		if (input.match(state) !== null) {
+			validInput = true;
+		} else {
 			validInput = false;
 		}
-		//return (validInput) that says whether valid or result or not and maybe one for
+		dir('valid state input =');
+		dir(validInput);
 		return validInput;
 	}
 
-/*--------------------- Execute the logic ---------------------*/
+	function isAddress(input) {
+		dir('entered isAddress ... input = ');
+		dir(input);
+		var validInput,
+			address = /^[0-9#\- ]+[A-Za-z0-9\.\-# ]+$/;
+		dir(input.match(address));
+		if (input.match(address) !== null) {
+			validInput = true;
+		} else {
+			validInput = false;
+		}
+		dir('valid address input =');
+		dir(validInput);
+		return validInput;
+	}
+
+	function isSimpleNumber(input) {
+		dir('entered isSimpleNumber ... input = ');
+		dir(input);
+		var validInput,
+			numbers = /^[0-9 ]+$/; // '+' assure there's at least one number in the string
+		dir(input.match(numbers));
+		if (input.match(numbers) !== null) {
+			validInput = true;
+		} else {
+			validInput = false;
+		}
+		dir('valid simple numeric input =');
+		dir(validInput);
+		return validInput;
+		//	/\d{5}(-\d{4})?/  //zip validation string
+	}
+
+	function isEmail(input) {
+		dir('entered isEmail ... input = ');
+		dir(input);
+		var validInput,
+			emails = /^[a-zA-Z_.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/; //works fine
+		// emails = /^[a-zA-Z_.]+@[a-zA-Z]+\.(com|edu|io)$/; //if I want to specify the domain explicitly
+		dir(input.match(emails));
+		if (input.match(emails) !== null) {
+			validInput = true;
+		} else {
+			validInput = false;
+		}
+		dir('valid email input =');
+		dir(validInput);
+		return validInput;
+	}
+
+	/*--------------------- Output to the Console ---------------------*/
+	function dir(arg) {
+		window.console.dir(arg);
+	}
+
+	/*--------------------- Execute the logic ---------------------*/
 
 	launchCustomer();
 
---
+++ js/training.js
@@ -0,0 +1,38 @@
+// Mario's training code 
+
+/*
+
+	var person = {
+	  firstname: 'Mario',
+	  lastname: 'Kaack',
+	  value: 100000,
+	  grades: [99, 88, 44],
+	  clothes: {
+		 pants: 'jeans',
+		 shirt: 't'
+	  },
+	  getFullName: function() {}
+	}
+
+
+	person.age = 44;
+
+	person.firstname;
+	person.grades[1];
+	person.clothes.shirt
+
+
+	var customerInfo = {};
+
+	myName.onblur = function() {
+	  var myInput = myName.value;
+	  // validate the input value
+	  ...
+	  if (!itvalidated) {
+		 customerInfo.myName = myInput
+	  } else {
+		 // html/css feedback
+	  }
+	  
+	  };
+	*/
